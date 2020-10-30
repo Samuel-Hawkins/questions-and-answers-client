8f7096c73366fcdb6e933b2edd72ca545f58ad1b
samuel-hawkins <samueledwardhawkins@gmail.com>
2020-10-30 14:57:59 -0400

    Added in production APIurl to heroku API

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://fast-basin-61102.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
